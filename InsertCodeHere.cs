0d235dc227bf9d498150e29158c8e507235ab74c
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-03-30 13:45:52 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WTF
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            
+
+            System.Console.WriteLine($"{MysticNumbers.ThreeNumbers(2,15,9)}, expected 15");
+            System.Console.WriteLine($"{MysticNumbers.TwoNumbers(7,20)}, expected 20");
+            System.Console.WriteLine($"{MysticNumbers.FourNumbers(7,20,52,12)}, expected 52");
+
+
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ MysticNumbers.cs
@@ -0,0 +1,113 @@
+﻿namespace WTF
+{
+    public class MysticNumbers
+    {
+        #region Methods
+        public static int ThreeNumbers(int a, int b, int c)
+        {
+            int result;
+
+            if (b > a)
+            {
+                result = b;
+                if (c > b)
+                {
+                    result = c;
+                }
+            }
+            else
+            {
+                result = a;
+                if (c > a)
+                {
+                    result = c;
+                }
+            }
+
+            return result;
+        } 
+        public static int TwoNumbers(int a, int b)
+        {
+            int result;
+
+            if (b > a)
+            {
+                result = b;
+            }
+            else
+            {
+                result = a;
+            }
+            return result;
+
+        }   
+        public static int FourNumbers(int a, int b, int c, int d)
+        {
+            int result;
+
+            if (TwoNumbers(a,b)>TwoNumbers(c,d))
+            {
+                result = TwoNumbers(a,b);
+            }
+            else 
+            {
+                result = TwoNumbers(c,d);
+            }
+
+            // if (a > b && a > c && a > d)
+            // {
+            //     result = a;
+            // }
+            // else if (b > a && b > c && b > d)
+            // {
+            //     result = b;
+            // }
+            // else if (c > a && c > b && c > d)
+            // {
+            //     result = c;
+            // }
+            // else
+            // {
+            //     result = d;
+            // }
+
+
+            // if(b > a)
+            // {
+            //     result = b;
+            //     if (c > b)
+            //     {
+            //         result = c;
+            //         if (d > c)
+            //         {
+            //             result = d;
+            //         }
+            //     }
+            // }
+            // else if(a > b) 
+            // {
+            //     result = a;
+            //     if (c > a)
+            //     {
+            //         result = c;
+            //         if (d >c)
+            //         {
+            //             result = d;
+            //         }
+            //     }
+            // }
+            // else
+            // {
+            //     result = d;
+            // }
+            return result;
+
+        }
+        public static int FourNumbers1(int a, int b, int c, int d)
+        {
+            return TwoNumbers(TwoNumbers(a,b),TwoNumbers(c,d));
+        }
+
+        #endregion
+    }
+}
